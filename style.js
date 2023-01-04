9544e6815869f6025734c97d3fef152436b107d2
Yosuf Bhuiyan <yosufbhuiyanbusiness@gmail.com>
2023-01-04 21:12:04 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,51 @@
+// function add (a,b){
+//     return a+b;
+// }
+// console.log("The answer is "+add( 25,35));
+
+
+// var add = function (e,f){
+//     return e-f;
+// }
+// var ami = add;
+
+
+
+// console.log( ami(33,56));
+
+
+// function eat(){
+
+//     console.log('I am sleeping now')
+// }
+// eat()
+
+
+function work(name , time){
+console.log(name +' start his work on ' + time);
+
+}
+
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
+work("Yosuf" ," 8:00am");
